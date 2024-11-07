66ddf272373cd2c1020c51c532dc522cd5c23803
strozw <strozw@users.noreply.github.com>
2024-11-07 09:13:27 +0900

    init

---
+++ .storybook/main.ts
@@ -0,0 +1,21 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-a11y",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/experimental-addon-test",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  features: {
+    experimentalRSC: true,
+  },
+};
+export default config;
---
+++ README.md
@@ -0,0 +1,45 @@
+# Demo App for Next.js with Storybook
+
+## Setup
+
+```
+pnpm install
+```
+
+## Tasks
+
+### Develop
+
+```
+pnpm dev
+```
+
+### Build APP
+
+```
+pnpm build
+```
+
+### Develop by Strobyook
+
+```
+pnpm storybook
+```
+
+### Build Strobyook
+
+```
+pnpm build-storybook
+```
+
+### Lint
+
+```
+pnpm test
+```
+
+### Test
+
+```
+pnpm test
+```
